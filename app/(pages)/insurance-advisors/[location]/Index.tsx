6857b46c1cf9e168f03e6f7558467987ee3e3b6c
Ajit Sharma <96855888+iamajitsharma@users.noreply.github.com>
2025-05-08 09:24:45 +0530

    Update Index.tsx

--- app/(pages)/insurance-advisors/[location]/Index.tsx
+++ app/(pages)/insurance-advisors/[location]/Index.tsx
@@ -61,7 +61,7 @@ export default function Index({ data }: { data: AdvisorType[] }) {
     } else {
       setAdvisorsData(data);
     }
-  }, [filterItems]);
+  }, [data, filterItems]);
 
   console.log(advisorsData, "Advisor List");
 
